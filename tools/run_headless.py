589169d08a91bf68ed50055e73ac7d7f4671be48
PatriotAri <ethan.ramsey98@gmail.com>
2025-12-20 01:36:07 -0600

    Add files via upload

--- tools/run_headless.py
+++ tools/run_headless.py
@@ -1,12 +1,12 @@
 from engine.core.engine import Engine
 from engine.core.clock import Clock
 from engine.ports.headless_window import HeadlessWindow
-from backends.pygame.renderer import PygameRenderer
+from engine.ports.null_renderer import NullRenderer
 
 
 def main():
     window = HeadlessWindow()
-    renderer = PygameRenderer()
+    renderer = NullRenderer()
     clock = Clock()
 
     engine = Engine(
